9d770863c2ed0c1c3bf2e32b63312c220395568a
hamzamughal <mughalh473@gmail.com>
2021-03-29 18:00:09 +0500

    navbar fixed functionality added

--- js/app.js
+++ js/app.js
@@ -10,3 +10,14 @@ const links = document.getElementById('nav-links')
 navBtn.addEventListener('click', () => {
     links.classList.toggle('show-links')
 })
+
+// nav fixed
+const navbar = document.querySelector('.navbar')
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 62) {
+        navbar.classList.add('fixed')
+    }
+    else {
+        navbar.classList.remove('fixed')
+    }
+})
