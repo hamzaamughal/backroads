8230018e7bf240db4ea4407e5458a9107ca0f047
hamzamughal <mughalh473@gmail.com>
2021-03-29 19:55:57 +0500

    final commit

--- js/app.js
+++ js/app.js
@@ -21,3 +21,34 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('fixed')
     }
 })
+// smoothh scroll
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault()
+        links.classList.remove('show-links')
+        const id = e.target.getAttribute('href').slice(1)
+        const element = document.getElementById(id)
+        //position
+        let position;
+        if (navbar.classList.contains('fixed')) {
+            position = element.offsetTop - 62
+        }
+        else {
+            position = element.offsetTop - 124;
+        }
+        if (window.innerWidth < 992) {
+            if (navbar.classList.contains('fixed')) {
+                position = element.offsetTop - 62;
+            } else {
+                position = element.offsetTop - 332 - 62;
+            }
+        }
+        // window scrollTo
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: "smooth"
+        })
+    })
+})
